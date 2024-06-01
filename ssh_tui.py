7dec628b7ea4c0d98f26c42dfb57bfae7bc98d7f
Ajay Ratnam <ajratnam.dev@gmail.com>
2024-06-01 23:14:00 +0530

    Standardizing UI

--- ssh_tui.py
+++ ssh_tui.py
@@ -3,12 +3,9 @@ import time
 from threading import Thread
 
 from blessed import Terminal
-from utils import draw, send
+from utils import draw, send, Layout
 
 term = Terminal()
-
-MOUSE_CLICK_ENABLE = "\x1b[?1000h\x1b[?1006h"
-MOUSE_CLICK_DISABLE = "\x1b[?1000l\x1b[?1006l"
 
 
 def parse_mouse_event(sequence):
@@ -28,65 +25,81 @@ def parse_mouse_event(sequence):
     return None
 
 
+# def handle_client2(chan):
+#     options = ["Username - ", "Password - "]
+#     values = ["", ""]
+#     selected_option_index = 0
+#     running = True
+#
+#     send(term.clear)
+#     draw((0, 0), term.bold(term.red('Welcome to the Pixel Platform! Please login to continue.')))
+#     send(term.hide_cursor)
+#     send(MOUSE_CLICK_ENABLE)
+#
+#     def do_blink():
+#         nonlocal chan
+#         blink = True
+#         while running:
+#             i = selected_option_index
+#             option = term.green("● ") + options[i] + term.lightcyan4(values[i] + ('|' if blink else ''))
+#             draw((2 + i, 0), option)
+#             blink = not blink
+#             time.sleep(.5)
+#
+#     Thread(target=do_blink).start()
+#
+#     try:
+#         while running:
+#             for i, option in enumerate(options):
+#                 if i == selected_option_index:
+#                     option = term.green("● ") + option + term.lightcyan4(values[i]) + term.lightcyan4("|")
+#                 else:
+#                     option = term.lightcyan4("○ " + option + term.lightcyan4(values[i]))
+#
+#                 draw((2 + i, 0), option)
+#
+#             draw((4 + i, 0), term.cyan(f'Press q to quit.'))
+#
+#             raw_data = chan.recv(1024)
+#             data = raw_data.decode('utf-8').strip()
+#             print(f"Received: {repr(data)}")
+#             if data.lower() == 'q':
+#                 break
+#             elif data == '\x1b[B':
+#                 selected_option_index = (selected_option_index + 1) % len(options)
+#             elif data == '\x1b[A':
+#                 selected_option_index = (selected_option_index - 1) % len(options)
+#             elif data == '':
+#                 if selected_option_index == 0:
+#                     selected_option_index = 1
+#             elif mouse := parse_mouse_event(data):
+#                 if mouse['event_type'] == 'press':
+#                     line = mouse['y'] - 3
+#                     if 0 <= line < len(options):
+#                         selected_option_index = line
+#             elif data.isprintable():
+#                 values[selected_option_index] += data
+#             elif data == '\x7f':
+#                 values[selected_option_index] = values[selected_option_index][:-1]
+#     finally:
+#         running = False
+#         send(term.normal_cursor)
+#         send(term.clear)
+#         send(MOUSE_CLICK_DISABLE)
+#         chan.close()
+
 def handle_client(chan):
-    options = ["Username - ", "Password - "]
-    values = ["", ""]
-    selected_option_index = 0
-    running = True
+    layout = Layout(chan, term)
 
-    send(term.clear)
-    draw((0, 0), term.bold(term.red('Welcome to the Pixel Platform! Please login to continue.')))
-    send(term.hide_cursor)
-    send(MOUSE_CLICK_ENABLE)
+    layout.add_label('Welcome to the Pixel Platform! Please login to continue.', term.red)
+    layout.add_gap(1)
+    layout.add_label("Username - ", term.color_rgb(122, 127, 255))
+    layout.add_label("Password - ", term.blue)
+    layout.add_gap(1)
+    layout.add_label('Press Q to Quit', term.pink)
 
-    def do_blink():
-        nonlocal chan
-        blink = True
-        while running:
-            i = selected_option_index
-            option = term.green("● ") + options[i] + term.lightcyan4(values[i] + ('|' if blink else ''))
-            draw((2 + i, 0), option)
-            blink = not blink
-            time.sleep(.5)
-
-    Thread(target=do_blink).start()
-
-    try:
-        while running:
-            for i, option in enumerate(options):
-                if i == selected_option_index:
-                    option = term.green("● ") + option + term.lightcyan4(values[i]) + term.lightcyan4("|")
-                else:
-                    option = term.lightcyan4("○ " + option + term.lightcyan4(values[i]))
-
-                draw((2 + i, 0), option)
-
-            draw((4 + i, 0), term.cyan(f'Press q to quit.'))
-
-            raw_data = chan.recv(1024)
-            data = raw_data.decode('utf-8').strip()
-            print(f"Received: {repr(data)}")
-            if data.lower() == 'q':
+    with layout:
+        while True:
+            layout.draw()
+            if chan.recv(1024).decode('utf-8').strip().lower() == "q":
                 break
-            elif data == '\x1b[B':
-                selected_option_index = (selected_option_index + 1) % len(options)
-            elif data == '\x1b[A':
-                selected_option_index = (selected_option_index - 1) % len(options)
-            elif data == '':
-                if selected_option_index == 0:
-                    selected_option_index = 1
-            elif mouse := parse_mouse_event(data):
-                if mouse['event_type'] == 'press':
-                    line = mouse['y'] - 3
-                    if 0 <= line < len(options):
-                        selected_option_index = line
-            elif data.isprintable():
-                values[selected_option_index] += data
-            elif data == '\x7f':
-                values[selected_option_index] = values[selected_option_index][:-1]
-    finally:
-        running = False
-        send(term.normal_cursor)
-        send(term.clear)
-        send(MOUSE_CLICK_DISABLE)
-        chan.close()
--- utils.py
+++ utils.py
@@ -2,21 +2,76 @@ import inspect
 import threading
 
 lock = threading.Lock()
+MOUSE_CLICK_ENABLE = "\x1b[?1000h\x1b[?1006h"
+MOUSE_CLICK_DISABLE = "\x1b[?1000l\x1b[?1006l"
 
 
-def draw(pos, text):
-    bf = inspect.currentframe().f_back
-    chan = bf.f_locals["chan"]
-    terminal = bf.f_globals["term"]
-
-    lock.acquire()
-    send(terminal.move_yx(*pos))
-    send(terminal.clear_eol())
-    send(text)
-    lock.release()
+def draw(pos, text, channel, terminal):
+    with lock:
+        send(terminal.move_yx(*pos), channel)
+        send(terminal.clear_eol(), channel)
+        send(text, channel)
 
 
-def send(text):
-    bf = inspect.currentframe().f_back
-    chan = bf.f_locals["chan"]
-    chan.send(text.encode())
+def send(text, channel):
+    channel.send(text)
+
+
+class Gap:
+    def __init__(self, count=1):
+        self.count = count
+
+
+class Label:
+    def __init__(self, text, color, channel, terminal):
+        self.text = text
+        self.color = color
+        self.channel = channel
+        self.terminal = terminal
+
+    def draw(self, pos):
+        fg_text = self.text
+        if self.color:
+            fg_text = self.color(self.text)
+        draw(pos, fg_text, self.channel, self.terminal)
+
+
+class Layout:
+    initialized = False
+
+    def __init__(self, channel, terminal):
+        self.channel = channel
+        self.terminal = terminal
+        self.elements = []
+
+    def add_label(self, text, color=None):
+        label = Label(text, color, self.channel, self.terminal)
+        self.elements.append(label)
+        return label
+
+    def add_gap(self, size):
+        gap = Gap(size)
+        self.elements.append(gap)
+        return gap
+
+    def draw(self):
+        i = 0
+        for element in self.elements:
+            if isinstance(element, Label) and not self.initialized:
+                element.draw((i, 0))
+            elif isinstance(element, Gap):
+                i += element.count - 1
+            i += 1
+        self.initialized = True
+
+    def __enter__(self):
+        send(self.terminal.clear, self.channel)
+        send(self.terminal.hide_cursor, self.channel)
+        send(MOUSE_CLICK_ENABLE, self.channel)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        send(self.terminal.normal_cursor, self.channel)
+        send(self.terminal.clear, self.channel)
+        send(MOUSE_CLICK_DISABLE, self.channel)
+        self.channel.close()
+
